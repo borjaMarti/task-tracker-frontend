76929c1b06e89d86b3d3fe8ca6c00a2fd110f9f6
Borja Martí <borjamarti@outlook.com>
2023-05-11 11:08:39 +0200

    added author

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,8 @@
-import { Link } from 'react-router-dom'
-
 function About() {
     return (
       <div>
           <h4>Version 1.0.0</h4>
           <span>made with ❤️ by <a href="https://borjamarti.netlify.app/">borjaMartí</a></span>
-          <Link to="/">Go Back</Link>
       </div>
     )
   }
