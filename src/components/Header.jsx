c8000d62d81149ab2abfc5013ea55dce3ebdd988
Borja Martí <borjamarti@outlook.com>
2023-05-11 11:16:06 +0200

    simplified Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 import { useLocation } from 'react-router-dom'
 
-function Header({ title, showAddTask, onAdd }) {
+function Header({ showAddTask, onAdd }) {
   const location = useLocation();
   
   return (
     <header className='header'>
-        <h1>{title}</h1>
-        {location.pathname === '/tasks' && ( 
+        <h1>Task Tracker</h1>
+        {location.pathname === '/' && ( 
         <Button 
           showAddTask={showAddTask} 
           onAdd={onAdd} 
@@ -17,19 +17,5 @@ function Header({ title, showAddTask, onAdd }) {
     </header>
   )
 }
-
-Header.defaultProps = {
-    title: 'Task Tracker',
-}
-
-Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
-
-// CSS in JS
-// const headingStyle = {
-//     color: 'red', 
-//     backgroundColor: 'black'
-// }
 
 export default Header
