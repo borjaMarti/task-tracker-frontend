cd78b1463f449f869e6afbac70750defa3f66d3b
Borja Martí <borjamarti@outlook.com>
2023-05-11 11:18:55 +0200

    fixed Tasks props

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -5,7 +5,7 @@ function Tasks({ tasks, onDelete, onToggle }) {
     <>
       {tasks.map((task) => (
         <Task 
-            key={task.id} 
+            key={task._id} 
             task={task} 
             onDelete={onDelete} 
             onToggle={onToggle} 
