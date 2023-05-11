efdcdeafeeac4ae279f0f1d66a2d03a137801f87
Borja Martí <borjamarti@outlook.com>
2023-05-11 11:11:09 +0200

    Link component in Footer now changes depending on location

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,17 @@
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 function Footer() {
+  const location = useLocation();
+
   return (
     <footer>
         <p>MIT 2023</p>
-        <Link to="/about">About</Link>
+        {
+          location.pathname === '/' ?
+          <Link to="/about">About</Link>
+          : <Link to="/">Go Back</Link>
+        }
     </footer>
   )
 }
